565667e4a18651d820cd7e00c93a4194d0e06b2d
Wolfgang Popp <mail@wolfgang-popp.de>
2016-06-25 12:52:17 +0200

    removed Android autogenerated code and android test cases

--- src/main/gen/de/unipassau/isl/evs/ssh/core/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package de.unipassau.isl.evs.ssh.core;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
